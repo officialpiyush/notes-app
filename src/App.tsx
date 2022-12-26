77cfb3bbd1a6d75a80fb6043c9df50b4197f54fa
Piyush <bhangalepiyush@gmail.com>
2022-12-26 18:43:08 +0530

    feat: add navbar with sign out button

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Suspense } from "react";
 import { useRoutes } from "react-router";
 import routes from "~react-pages";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="max-w-xl mx-auto">
-      <Suspense fallback={<div>Loading</div>}>
-        {useRoutes(routes)}
-      </Suspense>
+    <div>
+      <Navbar />
+      <div className="py-8 max-w-xl mx-auto">
+        <Suspense fallback={<div>Loading</div>}>{useRoutes(routes)}</Suspense>
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,46 @@
+import clsx from "clsx";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import nhost from "../utils/nhost";
+
+export default function Navbar() {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+
+  const checkAuth = async () => {
+    const isAuthenticated = await nhost.auth.isAuthenticatedAsync();
+
+    setLoggedIn(!!isAuthenticated);
+  };
+
+  const signOut = async () => {
+    await nhost.auth.signOut();
+  };
+
+  useEffect(() => {
+    checkAuth();
+  }, []);
+
+  return (
+    <nav>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <Link to="/" className="btn btn-ghost normal-case text-xl">
+            Notes
+          </Link>
+        </div>
+        <div className="flex-none">
+          <button
+            type="button"
+            onClick={signOut}
+            className={clsx(
+              "btn btn-sm glass btn-error",
+              isLoggedIn ? "block" : "hidden",
+            )}
+          >
+            Sign Out
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+}
