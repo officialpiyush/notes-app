afef39d7922c9cb1f06fb71a280e897e6f564949
Piyush <bhangalepiyush@gmail.com>
2022-12-26 15:38:36 +0530

    chore: add env intelisense

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pages/client-react" />
+
+interface ImportMetaEnv {
+  readonly VITE_NHOST_SUBDOMAIN: string;
+  readonly VITE_NHOST_REGION: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
