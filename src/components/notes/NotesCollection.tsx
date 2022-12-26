511120645d3eadf095881c10fb629b5b64312d12
Piyush <bhangalepiyush@gmail.com>
2022-12-26 22:55:05 +0530

    feat: note collection

---
+++ src/components/notes/NotesCollection.tsx
@@ -0,0 +1,24 @@
+import { useQuery } from "urql";
+import GET_NOTES from "../../utils/graphql/get";
+import NoteCard from "./NoteCard";
+
+export default function NotesCollection() {
+  const [{ data, fetching }] = useQuery({
+    query: GET_NOTES,
+  });
+
+  if (fetching) {
+    return <div>Loading...</div>;
+  }
+
+  if (data && data.notes && data.notes.length) {
+    return (
+      <div className="flex flex-col gap-2">
+        {data.notes.map((d: any) => (
+          <NoteCard key={d.id} title={d.title} id={d.id} />
+        ))}
+      </div>
+    );
+  }
+  return <div>You have no notes yet!</div>;
+}
