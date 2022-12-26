e257cda6d28f5f026aee9186939cee742eb96dd6
Piyush <bhangalepiyush@gmail.com>
2022-12-26 21:55:04 +0530

    feat: update generate input for custom classnames

--- src/components/GenerateInput.tsx
+++ src/components/GenerateInput.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import { Dispatch, SetStateAction } from "react";
 
 interface GenerateInputProps {
@@ -5,6 +6,8 @@ interface GenerateInputProps {
   state: string;
   setState: Dispatch<SetStateAction<string>>;
   type: string;
+  className?: string;
+  inputClassName?: string;
 }
 
 export default function GenerateInput({
@@ -12,9 +15,11 @@ export default function GenerateInput({
   state,
   setState,
   type,
+  className,
+  inputClassName,
 }: GenerateInputProps) {
   return (
-    <div>
+    <div className={className}>
       <label className="label" htmlFor={name}>
         <span className="label-text uppercase">{name}</span>
         <span className="label-text-alt text-red-400 uppercase">Required</span>
@@ -25,7 +30,10 @@ export default function GenerateInput({
         onChange={(event) => setState(event.currentTarget.value)}
         type={type}
         placeholder="Type here"
-        className="input input-bordered w-full max-w-xs"
+        className={clsx(
+          "input input-bordered w-full",
+          inputClassName ?? "max-w-xs",
+        )}
       />
     </div>
   );
