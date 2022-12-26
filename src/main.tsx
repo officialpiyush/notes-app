ca2d4eb75599461b6fe4448670d9ad54fc75d0d3
Piyush <bhangalepiyush@gmail.com>
2022-12-26 15:47:00 +0530

    feat: configure nhost

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,24 @@
+import { NhostProvider } from "@nhost/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+import nhost from "./utils/nhost";
+
 import "virtual:windi.css";
+import { NhostUrqlProvider } from "./providers/urlq";
 
 // eslint-disable-next-line no-undef
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <NhostProvider nhost={nhost}>
+        <NhostUrqlProvider nhost={nhost}>
+          <App />
+        </NhostUrqlProvider>
+      </NhostProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
---
+++ src/utils/nhost.ts
@@ -0,0 +1,6 @@
+import { NhostClient } from "@nhost/react";
+
+export default new NhostClient({
+  subdomain: import.meta.env.VITE_NHOST_SUBDOMAIN,
+  region: import.meta.env.VITE_NHOST_REGION,
+});
